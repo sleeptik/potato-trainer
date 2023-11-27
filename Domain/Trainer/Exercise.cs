95f4eb63ed0c50129262da75ec158481f824aaf7
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-27 14:43:11 +0500

    update exercise model
    add defaults to exercise, so rider doesnt remove private setter

--- Domain/Trainer/Exercise.cs
+++ Domain/Trainer/Exercise.cs
@@ -2,11 +2,11 @@
 
 public sealed class Exercise
 {
-    public int Id { get; private set; }
+    public int Id { get; private set; } = default;
     public string Contents { get; private set; } = null!;
 
-    public int DifficultyId { get; private set; }
+    public int DifficultyId { get; private set; } = default;
     public Difficulty Difficulty { get; private set; } = null!;
-    
+
     public IList<Subject> Subjects { get; private set; } = new List<Subject>();
 }
